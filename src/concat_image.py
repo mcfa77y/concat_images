9452b0db20769fb79c7a4c2947de447304546f46
Joe Lau <mcfa77y@gmail.com>
2018-12-03 22:13:10 -0800

    updates on comments

--- src/concat_image.py
+++ src/concat_image.py
@@ -44,16 +44,21 @@ def merge_images(image_uri_array, output_image_uri):
     os.system(concat_images_command)
 
 def merge_images_PIL(image_uri_array, output_image_uri):  
-    x_offset = 100
-    y_offset = 10
-    x_between = 30
+    # space on left and right of merged images
+    x_offset = 0
+    # space on top and below merged images
+    y_offset = 0
+    # space between images
+    x_between = 10
 
+    # open images and collect heights and widths to calc max width
     images = map(Image.open, image_uri_array)
     widths, heights = zip(*(i.size for i in images))
 
     total_width = sum(widths) + (len(image_uri_array)) * x_offset
     max_height = max(heights) + 2 * y_offset
 
+    # create new image to hold merge images
     new_im = Image.new('RGB', (total_width, max_height), ImageColor.getrgb('white'))
 
     # have to get images again because they close after initial use
@@ -68,19 +73,26 @@ def merge_images_PIL(image_uri_array, output_image_uri):
 def label_image_PIL(label, image_uri, output_image_uri=None):        
     
     with Image.open(image_uri) as img:
-        label_height = 30
+        label_height = 100
+        font_size = 100
+
         # make a blank image for the text
         label_img = Image.new('RGBA', (img.width, label_height), ImageColor.getrgb('white'))
 
         # get a font
-        fnt = ImageFont.truetype(FONT_URI, 40)
+        fnt = ImageFont.truetype(FONT_URI, font_size)
         # get a drawing context
         label_draw = ImageDraw.Draw(label_img)
-        label_draw.text((10,10),label, font=fnt)
+        label_draw.text((10,10),label, font=fnt,fill=(0,0,255,128))
 
+        # image holder 
         new_img = Image.new('RGB', (img.width, img.height + label_img.height), ImageColor.getrgb('white'))
+        # paste image in image holder
         new_img.paste(img)
+        # paste label in image holder
         new_img.paste(label_img, (0, img.height))
+
+        # save image
         if (output_image_uri == None):
             new_img.save(image_uri)
         else:
